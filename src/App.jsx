c5007adaa343cfe27af1d8067b6598f5479ae228
RyuChaewon <159004046+RyuChaewon@users.noreply.github.com>
2026-08-19 20:01:15 +0900

    docs: App 주석 정리

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import useImagePreloader from './hooks/useImagePreloader';
 
-// [1] 기능 컴포넌트 불러오기
+// 공통 기능 컴포넌트
 import ScrollToTop from './components/ScrollToTop';
-import BackgroundMusic from './components/BackgroundMusic'; // 새로 추가!
+import BackgroundMusic from './components/BackgroundMusic';
 
-// --- [2] 이미지 에셋 미리 불러오기 ---
+// 페이지 전환에 사용되는 배경 이미지
 import bgWrite from './assets/bg-write.svg';
 import bgEntrance from './assets/bg-entrance.svg';
 import bgLanding from './assets/bg-landing.svg';
@@ -17,7 +17,7 @@ import bgOutro from './assets/bg-outro.svg';
 import bgStable from './assets/bg-stable.svg';
 import bgStableRead from './assets/bg-stable-read.svg';
 
-// 페이지들 불러오기
+// 라우트 페이지
 import LandingPage from './pages/LandingPage';
 import EntrancePage from './pages/EntrancePage';
 import MainPage from './pages/MainPage';
@@ -29,7 +29,7 @@ import MessageDetail from './pages/MessageDetail';
 import OutroPage from './pages/OutroPage';
 
 function App() {
-  // --- [3] 프리로딩할 이미지 리스트 ---
+  // 주요 배경 이미지를 미리 불러와 화면 전환 시 깜빡임을 줄입니다.
   const imagesToPreload = [
     bgLanding,
     bgEntrance,
@@ -42,19 +42,14 @@ function App() {
     bgStableRead
   ];
 
-  // --- [4] 훅 실행 ---
   useImagePreloader(imagesToPreload);
 
   return (
     <BrowserRouter>
-      {/* [기능 1] 페이지 이동 시 스크롤 맨 위로 
-        Routes 바깥에 있어야 모든 페이지에 적용됨
-      */}
+      {/* 모든 페이지 이동 후 스크롤 위치를 상단으로 초기화합니다. */}
       <ScrollToTop />
 
-      {/* [기능 2] 배경 음악 재생 (4곡 반복)
-        Routes 바깥에 있어야 페이지 이동해도 끊기지 않음
-      */}
+      {/* 페이지가 바뀌어도 배경 음악이 이어지도록 라우트 바깥에서 재생합니다. */}
       <BackgroundMusic />
 
       <Routes>
@@ -62,17 +57,17 @@ function App() {
         <Route path="/entrance" element={<EntrancePage />} />
         <Route path="/main" element={<MainPage />} />
         
-        {/* 리스트 페이지 */}
+        {/* 메시지 목록 */}
         <Route path="/sky" element={<NightSkyList />} />
         <Route path="/stable" element={<StableList />} />
 
-        {/* 글쓰기 */}
+        {/* 메시지 작성 */}
         <Route path="/:theme/write" element={<MessageForm />} />
 
         {/* 오너먼트 선택 */}
         <Route path="/select/:theme" element={<OrnamentSelect />} />
         
-        {/* 상세 보기 */}
+        {/* 메시지 상세 보기 */}
         <Route path="/:theme/detail/:id" element={<MessageDetail />} />
 
         <Route path="/outro" element={<OutroPage />} />
